88fd7a7fcb760fbd65c18d4d2db37ea4cfbb22f6
bgarciabtk <bgarcia@btkfarma.com>
2025-04-08 14:29:25 -0600

    set interfaec to coin element an update background page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,43 @@
-'use client';
+"use client";
 
-import { useQuery } from '@tanstack/react-query';
-import { getTopCoins } from '@/lib/coingecko';
+import { useQuery } from "@tanstack/react-query";
+import { getTopCoins } from "@/lib/coingecko";
+import KPI from "@/components/KPI";
+import CryptoChart from "@/components/CryptoChart";
+import CryptoTable from "@/components/CryptoTable";
+import { CoinElementInterface } from "./interface";
 
 export default function DashboardPage() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['topCoins'],
+  const {
+    data: coins,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["topCoins"],
     queryFn: getTopCoins,
   });
 
   if (isLoading) return <div className="p-8">Cargando criptos...</div>;
-  if (error) return <div className="p-8 text-red-500">Error al cargar datos</div>;
+  if (error)
+    return <div className="p-8 text-red-500">Error al cargar datos</div>;
 
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Dashboard de Criptomonedas</h1>
-      <pre className="bg-gray-100 p-4 rounded">
-        {JSON.stringify(data?.slice(0, 3), null, 2)}
-      </pre>
+    <div className="p-8 space-y-6 background-ligth">
+      <h1 className="text-3xl font-bold">Dashboard de Criptomonedas</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {coins.slice(0, 3).map((coin: CoinElementInterface) => (
+          <KPI
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            change={coin.price_change_percentage_24h}
+          />
+        ))}
+      </div>
+
+      <CryptoChart data={coins[0]} />
+      <CryptoTable coins={coins} />
     </div>
   );
 }
