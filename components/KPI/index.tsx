4b43a739013324d97654deced97c3ba8fca3d5dc
bgarciabtk <bgarcia@btkfarma.com>
2025-04-08 13:16:26 -0600

    update classes to prices and background

--- components/KPI/index.tsx
+++ components/KPI/index.tsx
@@ -9,12 +9,12 @@ export const KPI = ({ name, price, change }: KPIProps) => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4 }}
-      className="bg-x2light p-6 rounded-2xl  transition-all"
+      className="bg-x2dark p-6 rounded-2xl  transition-all"
     >
       <h2 className="text-xl font-semibold">{name}</h2>
       <p className="text-2xl font-bold">${price.toLocaleString()}</p>
       <p
-        className={`text-sm ${change >= 0 ? "text-green-500" : "text-red-500"}`}
+        className={`text-sm ${change >= 0 ? "green-price" : "red-price"}`}
       >
         {change >= 0 ? "+" : ""}
         {change.toFixed(2)}%
