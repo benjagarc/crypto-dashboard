ea902eaa4980b3dd553d2d33b66a902df3c21138
bgarciabtk <bgarcia@btkfarma.com>
2025-04-08 17:57:12 -0600

    set stale time and update condition to show content

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTopCoins } from "@/lib/coingecko";
 import KPI from "@/components/KPI";
-import CryptoChart from "@/components/CryptoChart";
+// import CryptoChart from "@/components/CryptoChart";
 import CryptoTable from "@/components/CryptoTable";
 import { CoinElementInterface } from "./interface";
 import Filters from "@/components/Filters";
@@ -11,9 +11,13 @@ import {
   typeChangeRange,
   typePriceRange,
 } from "@/components/Filters/interface";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import SearchBar from "@/components/SearchBar";
-
+import dynamic from "next/dynamic";
+const CryptoChart = dynamic(() => import("@/components/CryptoChart"), {
+  ssr: false,
+  loading: () => <p>Loading Chart...</p>,
+});
 export default function DashboardPage() {
   const {
     data: coins,
@@ -22,6 +26,7 @@ export default function DashboardPage() {
   } = useQuery({
     queryKey: ["topCoins"],
     queryFn: getTopCoins,
+    staleTime: 1000 * 60,
   });
 
   const [filteredCoins, setFilteredCoins] = useState<CoinElementInterface[]>(
@@ -72,12 +77,26 @@ export default function DashboardPage() {
     }
   };
 
+  const TopCoins = useMemo(() => {
+    return filteredCoins?.length > 0 ? filteredCoins?.slice(0, 3) : [];
+  }, [filteredCoins]);
+
+  useEffect(() => {
+    setFilteredCoins(() => coins);
+  }, [coins]);
+
   // components
 
   if (isLoading)
-    return <div className="p-8 background-purple bg-x2dark">Cargando criptos...</div>;
+    return (
+      <div className="p-8 background-purple bg-x2dark">Loading criptos...</div>
+    );
   if (error)
-    return <div className="p-8 text-red-500 background-purple">Error al cargar datos</div>;
+    return (
+      <div className="p-8 text-red-500 background-purple">
+        Error to load data
+      </div>
+    );
 
   return (
     <div className="p-8 space-y-6 background-purple">
@@ -90,7 +109,7 @@ export default function DashboardPage() {
       <SearchBar onSearch={handleSearch} />
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-        {filteredCoins.slice(0, 3).map((coin: CoinElementInterface) => (
+        {TopCoins.slice(0, 3).map((coin: CoinElementInterface) => (
           <KPI
             key={coin.id}
             name={coin.name}
@@ -100,7 +119,7 @@ export default function DashboardPage() {
         ))}
       </div>
 
-      <CryptoChart data={coins[0]} />
+      {filteredCoins?.length > 0 && <CryptoChart data={filteredCoins[0]} />}
       <CryptoTable coins={coins} />
     </div>
   );
